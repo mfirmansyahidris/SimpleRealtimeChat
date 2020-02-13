86856e6969fef620738df5c2a8847da7e1219e9a
Manca fi <manca.fi@gmail.com>
2020-02-13 12:49:37 +0800

    android: config toolbar

--- android/SimpleRealtimeChat/app/src/main/java/com/fi/simplerealtimechat/screen/MainActivity.kt
+++ android/SimpleRealtimeChat/app/src/main/java/com/fi/simplerealtimechat/screen/MainActivity.kt
@@ -1,18 +1,117 @@
 package com.fi.simplerealtimechat.screen
 
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
 import com.fi.simplerealtimechat.R
 import com.fi.simplerealtimechat.base.BaseActivity
+import com.fi.simplerealtimechat.utils.PrefManager
+import kotlinx.android.synthetic.main.toolbar_activity_main.*
 
 class MainActivity : BaseActivity() {
-    override fun getLayoutResource(): Int =
-        R.layout.activity_main
+    private var menu: Menu? = null
+    private lateinit var menuItem: MenuItem
+    private lateinit var prefManager: PrefManager
 
-    override fun getToolbarResource(): Int = 0
+    override fun getLayoutResource(): Int = R.layout.activity_main
+
+    override fun getToolbarResource(): Int = R.id.toolbar_mainActivity
 
     override fun getToolbarTitle(): String = ""
 
     override fun mainCode() {
+        prefManager = PrefManager(this)
 
+        setToolbar()
+
+        setHomeButton(false)
+
+        ll_userName.visibility = View.GONE
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main_activity, menu)
+        this.menu = menu
+
+        this.menu?.let {
+            menuItem = it.findItem(R.id.menu_check)
+            menuItem.setVisible(false)
+        }
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return when (item?.itemId) {
+            android.R.id.home -> {
+                clearToolbarState("")
+                true
+            }
+            R.id.menu_check -> {
+                clearToolbarState(et_userName.text.toString())
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun setToolbarState() {
+        setHomeButton(true)
+        ll_userName.visibility = View.VISIBLE
+
+        if (prefManager.getName().isBlank()) {
+            et_userName.setText(getString(R.string.main_anonymous))
+        } else {
+            et_userName.setText(prefManager.getName())
+        }
+
+        et_userName.requestFocus()
+        et_userName.setSelection(et_userName.text.toString().length)
+
+        menuItem.isVisible = true
+    }
+
+    private fun clearToolbarState(userName: String) {
+        setHomeButton(false)
+        ll_userName.visibility = View.GONE
+
+        prefManager.setName(userName)
+
+        et_userName.setText(getUserName())
+
+        toolbar?.title = getUserName()
+
+        menuItem.isVisible = false
+    }
+
+    private fun setHomeButton(state: Boolean) {
+        supportActionBar?.setDefaultDisplayHomeAsUpEnabled(state)
+        supportActionBar?.setDisplayHomeAsUpEnabled(state)
+    }
+
+    private fun setToolbar() {
+        toolbar = findViewById(getToolbarResource())
+        toolbar?.setNavigationIcon(R.drawable.ic_close)
+        toolbar?.title = getToolbarTitle()
+        setSupportActionBar(toolbar)
+
+        toolbar?.title = getUserName()
+
+        toolbar?.setOnClickListener {
+            setToolbarState()
+        }
+
+        toolbar?.subtitle = getString(R.string.main_subtitle)
+    }
+
+    private fun getUserName(): String {
+        return if (prefManager.getName().isBlank()) {
+            getString(R.string.main_anonymous)
+        } else {
+            prefManager.getName()
+        }
     }
 
 }
---
+++ android/SimpleRealtimeChat/app/src/main/java/com/fi/simplerealtimechat/utils/PrefManager.kt
@@ -0,0 +1,48 @@
+package com.fi.simplerealtimechat.utils
+
+import android.content.Context
+import android.content.SharedPreferences
+import android.util.Log
+
+/**
+ ****************************************
+created by -fi-
+.::manca.fi@gmail.com ::.
+
+13/02/2020, 11:17 AM
+ ****************************************
+ */
+
+class PrefManager
+constructor(context: Context) {
+    private val sharedPref: SharedPreferences
+
+    init {
+        sharedPref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
+    }
+
+    private fun setString(key: String, value: String?) {
+        val editor = sharedPref.edit()
+        editor.putString(key, value)
+        editor.apply()
+    }
+
+    private fun getString(key: String): String? = sharedPref.getString(key, "")
+
+    fun setName(name: String){
+        setString(userName, name)
+    }
+
+    fun getName(): String {
+        return getString(userName).toString()
+    }
+
+    fun clear() {
+        sharedPref.edit().clear().apply()
+    }
+
+    companion object {
+        private const val prefName = "SimpleRealtimeChat"
+        private const val userName = "userName"
+    }
+}
