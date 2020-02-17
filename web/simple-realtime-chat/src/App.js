cacbeb8d1917c71ea4f4f34362d9751eb50faedc
Manca fi <manca.fi@gmail.com>
2020-02-17 15:48:46 +0800

    web: add awesome font and expand more

--- web/simple-realtime-chat/src/App.js
+++ web/simple-realtime-chat/src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap'
 import './App.css'
-import RoomsContainer from './containers/rooms/RoomsContainer';
 import ChatsContainer from './containers/chats/ChatsContainer';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPaperPlane } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
   return (
@@ -10,11 +12,18 @@ function App() {
       <div className="container">
         <h3 className="text-center">Messaging</h3>
         <div className="messaging">
-          <div className="inbox_msg">
-            <RoomsContainer />
+          <div className="inbox_msg">  
+            <div className="mesgs">
 
-            <ChatsContainer />
+              <ChatsContainer />
 
+              <div className="type_msg">                
+                <div className="input_msg_write">
+                  <input type="text" class="write_msg" placeholder="Type a message" />
+                  <button className="msg_send_btn" type="button"><FontAwesomeIcon icon={faPaperPlane}/></button>
+                </div>
+              </div>
+            </div>
             
           </div>
         </div>
--- web/simple-realtime-chat/src/containers/chats/ChatsContainer.jsx
+++ web/simple-realtime-chat/src/containers/chats/ChatsContainer.jsx
@@ -5,26 +5,17 @@ import ChatOutgoingComponent from "../../components/chats/ChatOutgoingComponent"
 class ChatsContainer extends Component{
     render(){
         return(
-            <div className="mesgs">
-              <div className="msg_history">
+          <div className="msg_history">
                 
-                <ChatIncomingComponent />
+            <ChatIncomingComponent />
 
-                <ChatOutgoingComponent />
+            <ChatOutgoingComponent />
 
-                <ChatIncomingComponent />
+            <ChatIncomingComponent />
 
-                <ChatOutgoingComponent />
-                
-              </div>
-
-              <div className="type_msg">
-                <div className="input_msg_write">
-                  <input type="text" class="write_msg" placeholder="Type a message" />
-                  <button class="msg_send_btn" type="button"><i class="fa fa-paper-plane-o" aria-hidden="true"></i></button>
-                </div>
-              </div>
-            </div>
+            <ChatOutgoingComponent />
+            
+          </div>
         )
     }
 }
