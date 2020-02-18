ecdcf960294ca34257b2e048b7ab25b4939428a7
Manca fi <manca.fi@gmail.com>
2020-02-18 15:31:01 +0800

    web: firebase config

--- web/simple-realtime-chat/src/containers/chats/ChatsContainer.jsx
+++ web/simple-realtime-chat/src/containers/chats/ChatsContainer.jsx
@@ -2,31 +2,31 @@ import React, { Component } from "react";
 import ChatComponent from "../../components/chats/ChatsComponent";
 import './ChatsContainer.css'
 
-class ChatsContainer extends Component{
-    render(){
-        return(
-          <div className="msg_history">
+class ChatsContainer extends Component {
+  render() {
+    return (
+      <div className="msg_history">
 
-            <ChatComponent
-              sender="Ali"
-              message="Haii.."
-              time="10:30"
-            />
-            <ChatComponent
-              sender="Bambang"
-              message="Ali selalu kencing berdiri"
-              time="10:30"
-            />
-            <ChatComponent
-              sender="Ali"
-              message="Bahwa sesungguhnya kemerdekaan itu ialah hak segala bangsa dan oleh sebab itu maka penjajahan di atas dunia harus dihapuskan"
-              time="10:30"
-            
-            />
-            
-          </div>
-        )
-    }
+        <ChatComponent
+          sender="Ali"
+          message="Haii.."
+          time="10:30"
+        />
+        <ChatComponent
+          sender="Bambang"
+          message="Ali selalu kencing berdiri"
+          time="10:30"
+        />
+        <ChatComponent
+          sender="Ali"
+          message="Bahwa sesungguhnya kemerdekaan itu ialah hak segala bangsa dan oleh sebab itu maka penjajahan di atas dunia harus dihapuskan"
+          time="10:30"
+
+        />
+
+      </div>
+    )
+  }
 }
 
 export default ChatsContainer
