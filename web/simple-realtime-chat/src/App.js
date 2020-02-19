1526b7e975670d633187f215abc4267bc17be407
Manca fi <manca.fi@gmail.com>
2020-02-19 18:52:53 +0800

    web: add user name and update style

--- web/simple-realtime-chat/src/App.js
+++ web/simple-realtime-chat/src/App.js
@@ -1,35 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap'
 import './App.css'
 import ChatsContainer from './containers/chats/ChatsContainer';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPaperPlane } from '@fortawesome/free-solid-svg-icons'
+import TypeMsgComponent from './components/typeMsg/TypeMsgComponent';
 
-function App() {
-  return (
-    <div className="App">
-      <div className="container">
-        <h3 className="text-center">Messaging</h3>
-        <div className="messaging">
-          <div className="inbox_msg">  
-            <div className="mesgs">
 
-              <ChatsContainer />
+class App extends Component{
+  constructor(props){
+    super(props)
 
-              <div className="type_msg">                
-                <div className="input_msg_write">
-                  <input type="text" className="write_msg" placeholder="Type a message" />
-                  <button className="msg_send_btn" type="button"><FontAwesomeIcon icon={faPaperPlane}/></button>
-                </div>
+    this.state = {
+      userName: ''
+    }
+  }
+
+  handleUserNameChange = (newUserName) => {
+    this.setState({
+      userName: newUserName
+    })
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <div className="container">
+          <h3 className="text-center">Messaging</h3>
+          <div className="messaging">
+            <div className="inbox_msg">  
+              <div className="mesgs">
+  
+                <ChatsContainer />
+  
+                <TypeMsgComponent onUserNameChange={this.handleUserNameChange} />
+  
               </div>
+              
             </div>
-            
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
-export default App;
+export default App
---
+++ web/simple-realtime-chat/src/components/typeMsg/TypeMsgComponent.jsx
@@ -0,0 +1,71 @@
+import React, { Component } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPaperPlane, faPen } from '@fortawesome/free-solid-svg-icons'
+import './TypeMsgComponent.css'
+import { Modal, Button } from "react-bootstrap";
+
+class TypeMsgComponent extends Component{
+    constructor(props){
+        super(props)
+
+        this.state = {
+            userName: '',
+            userNameDialogShow: false,
+        }
+    }
+
+    handelUserNameDialogClose = () => this.setState({userNameDialogShow: false})
+
+    handleUserNameDialogShow = () => this.setState({userNameDialogShow: true})
+
+    handleUserNameChange = (e) => this.setState({userName: e.target.value})
+
+    handleSaveUserNameButton = () => {
+        this.handelUserNameDialogClose()
+        this.props.onUserNameChange(this.state.userName)
+    }
+    render(){
+        return(
+            <div className="type_msg">           
+                <div className="user_name">
+                    <h6>Anonymouse</h6>
+                    <button 
+                        onClick={this.handleUserNameDialogShow} 
+                        className="user_name_edit_btn" 
+                        type="button">
+                            <FontAwesomeIcon icon={ faPen } />
+                    </button>
+                </div>
+                <div className="input_msg_write">
+                    <input type="text" className="write_msg" placeholder="Type a message" />
+                    <button className="msg_send_btn" type="button"><FontAwesomeIcon icon={faPaperPlane}/></button>
+                </div>
+
+                <Modal show={this.state.userNameDialogShow} onHide={this.handelUserNameDialogClose}>
+                    <Modal.Header>
+                        <Modal.Title>Set User Name</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <input 
+                            type="text" 
+                            value={this.state.userName} 
+                            onChange={this.handleUserNameChange} 
+                            className="new_user_name" 
+                            placeholder="Set Awesome User Name"
+                        />
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={this.handelUserNameDialogClose}>
+                            Close
+                        </Button>
+                        <Button variant="primary" onClick={this.handleSaveUserNameButton}>
+                            Save Changes
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+            </div>
+        )
+    }
+}
+
+export default TypeMsgComponent
