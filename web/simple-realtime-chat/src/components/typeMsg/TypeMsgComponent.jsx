1b46b0c2fc3e85b7cc997018e2386226dd12d019
Manca fi <manca.fi@gmail.com>
2020-02-20 19:54:37 +0800

    web: chat data model fit with real time database in firebase.

--- web/simple-realtime-chat/src/components/typeMsg/TypeMsgComponent.jsx
+++ web/simple-realtime-chat/src/components/typeMsg/TypeMsgComponent.jsx
@@ -4,6 +4,8 @@ import { faPaperPlane, faPen } from '@fortawesome/free-solid-svg-icons'
 import './TypeMsgComponent.css'
 import { Modal, Button } from "react-bootstrap";
 import { colorRand } from '../../config/Colors'
+import FirebaseInit from '../../config/FirebaseInit'
+var dateformat = require('dateformat')
 
 class TypeMsgComponent extends Component{
     constructor(props){
@@ -13,6 +15,7 @@ class TypeMsgComponent extends Component{
             userName: props.userName,
             color: props.color,
             userNameDialogShow: false,
+            message: '',
 
             newUserName: props.userName,
             newColor: ''
@@ -27,6 +30,12 @@ class TypeMsgComponent extends Component{
         this.setState({userNameDialogShow: false})
     }
 
+    handleOnKeyPressMessage = (e) => {
+        if(e.key === 'Enter'){
+            this.handleSend()
+        }
+    }
+
     handleSaveUserNameButton = () => {
         let color = colorRand
         this.handelUserNameDialogClose()
@@ -37,6 +46,21 @@ class TypeMsgComponent extends Component{
         })
     }
 
+    handleMessageChange = (e) => this.setState({message: e.target.value})
+
+    handleSend = () => {
+        let ref = FirebaseInit.database().ref('/chats')
+        ref.child(Date.now()).set({
+            color: this.state.color,
+            message: this.state.message,
+            from: this.state.userName,
+            time: dateformat(Date.now(), "dd mmm yyyy, hh:MM")
+        })
+
+
+        this.setState({message: ''})
+    }
+
     componentDidMount(){
         this.setState({
             userName: this.state.userName
@@ -45,7 +69,7 @@ class TypeMsgComponent extends Component{
 
     render(){
         return(
-            <div className="type_msg">           
+            <div className="type_msg">   
                 <div className="user_name">
                     <h6>{this.state.userName}</h6>
                     <button 
@@ -57,8 +81,18 @@ class TypeMsgComponent extends Component{
                     </button>
                 </div>
                 <div className="input_msg_write">
-                    <input type="text" className="write_msg" placeholder="Type a message" />
-                    <button className="msg_send_btn" type="button"><FontAwesomeIcon icon={faPaperPlane}/></button>
+                    <input 
+                        type="text" 
+                        className="write_msg"
+                        onKeyPress={this.handleOnKeyPressMessage} 
+                        onChange={this.handleMessageChange}
+                        value={this.state.message} 
+                        placeholder="Type a message" />
+                    <button
+                        className="msg_send_btn" 
+                        type="button"
+                        onClick={this.handleSend}
+                    ><FontAwesomeIcon icon={faPaperPlane}/></button>
                 </div>
 
                 <Modal show={this.state.userNameDialogShow} onHide={this.handelUserNameDialogClose}>
